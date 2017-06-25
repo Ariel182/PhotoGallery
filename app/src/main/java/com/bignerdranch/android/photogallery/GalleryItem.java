ea57dd1bdca2f1f5ff34fcd3db3fd63975c77f4f
Ariel Pereira Gadea <ariel_1_8_2@yahoo.es>
2017-06-25 18:04:38 -0300

    Fin capitulo 30

--- app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.photogallery;
 
+import android.net.Uri;
+
 public class GalleryItem {
     public String getCaption() {
         return mCaption;
@@ -28,10 +30,27 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     @Override
     public String toString() {
         return mCaption;
     }
+
+	public String getOwner() {
+		return mOwner;
+	}
+
+	public void setOwner(String owner) {
+		mOwner = owner;
+	}
+
+	public Uri getPhotoPageUri() {
+		return Uri.parse("https://www.flickr.com/photos/")
+				.buildUpon()
+				.appendPath(mOwner)
+				.appendPath(mId)
+				.build();
+	}
 }
 
